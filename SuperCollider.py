b2467b79e06454456ccd873112a27ce1351fc3b4
Arthur Carabott <arthurc@gmail.com>
2014-10-30 09:27:52 +0200

    move running process to separate class, add stop

--- SuperCollider.py
+++ SuperCollider.py
@@ -9,51 +9,73 @@ def enqueue_output(out, queue):
         queue.put(line)
     out.close()
 
-class SuperColliderStartCommand(sublime_plugin.ApplicationCommand):
+class SuperColliderProcess():
     sclang_thread = None
     sclang_process = None
     sclang_queue = None
     sclang_thread = None
 
+    def start():
+        if SuperColliderProcess.isAlive():
+            return
+
+        settings = sublime.load_settings("SuperCollider.sublime-settings")
+        sc_dir = settings.get("sc_dir")
+        sc_exe = settings.get("sc_exe")
+
+        args = ['', '-i', 'sublime']
+        cwd = None
+        close_fds = None
+        shell = None
+
+        if os.name is 'posix':
+            args[0] = sc_dir + sc_exe
+            close_fds = True
+            shell = False
+        else:
+            args[0] = sc_exe
+            cwd = sc_dir
+            close_fds = False
+            shell = True
+
+        SuperColliderProcess.sclang_process = subprocess.Popen(
+            args = args,
+            cwd = cwd,
+            bufsize = 0,
+            stdin = subprocess.PIPE,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.STDOUT,
+            close_fds = close_fds,
+            shell = shell
+        )
+
+        SuperColliderProcess.sclang_queue = Queue()
+        SuperColliderProcess.sclang_thread = threading.Thread(
+            target = enqueue_output,
+            args = (SuperColliderProcess.sclang_process.stdout,
+                    SuperColliderProcess.sclang_queue)
+        )
+        SuperColliderProcess.sclang_thread.daemon = True #dies with the program
+        SuperColliderProcess.sclang_thread.start()
+        sublime.status_message("Starting SuperCollider")
+
+    def isAlive():
+        return (SuperColliderProcess.sclang_thread is not None and
+                SuperColliderProcess.sclang_thread.isAlive())
+
+    def execute(cmd):
+        SuperColliderProcess.sclang_process.stdin.write(bytes(cmd, 'utf-8'))
+        SuperColliderProcess.sclang_process.stdin.write(bytes("\x0c", 'utf-8'))
+        SuperColliderProcess.sclang_process.stdin.flush()
+
+class SuperColliderStartCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        # start supercollider
-        if self.sclang_thread is None or not sclang_thread.isAlive():
-            settings = sublime.load_settings("SuperCollider.sublime-settings")
-            sc_dir = settings.get("sc_dir")
-            sc_exe = settings.get("sc_exe")
+        SuperColliderProcess.start()
 
-            args = ['', '-i', 'sublime']
-            cwd = None
-            close_fds = None
-            shell = None
-
-            if os.name is 'posix':
-                args[0] = sc_dir + sc_exe
-                close_fds = True
-                shell = False
-            else:
-                args[0] = sc_exe
-                cwd = sc_dir
-                close_fds = False
-                shell = True
-
-            self.sclang_process = subprocess.Popen(
-                args = args,
-                cwd = cwd,
-                bufsize = 0,
-                stdin = subprocess.PIPE,
-                stdout = subprocess.PIPE,
-                stderr = subprocess.STDOUT,
-                close_fds = close_fds,
-                shell = shell
-            )
-
-            self.sclang_queue = Queue()
-            self.sclang_thread = threading.Thread(
-                target=enqueue_output,
-                args=(self.sclang_process.stdout, self.sclang_queue)
-            )
-            self.sclang_thread.daemon = True # thread dies with the program
-            self.sclang_thread.start()
-            sublime.status_message("Starting SuperCollider")
-
+class SuperColliderStopCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        if SuperColliderProcess.isAlive():
+            SuperColliderProcess.execute("0.exit;")
+            sublime.status_message("Stopped SCLang")
+        else:
+            sublime.status_message("SCLang not started")
