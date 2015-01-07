27c0e2969c1bee1cfb70d451d1d6d624c417e30b
Arthur Carabott <arthurc@gmail.com>
2015-01-07 02:22:26 +0000

    only exec on_window if sc not None

--- SuperCollider.py
+++ SuperCollider.py
@@ -530,6 +530,8 @@ class SuperColliderListener(sublime_plugin.EventListener):
             sc.remove_post_view()
 
     def on_window_command(self, window, command_name, args):
+        if sc is None:
+            return
         if command_name == "hide_panel":
             value = sc.post_view.substr(sublime.Region(0, sc.post_view.size()))
             sc.cache_post_view(value)
